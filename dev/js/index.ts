b2cf27d72f944776d1323387cb5f48fd28659b53
Siarhej <serjukbrest@gmail.com>
2022-09-14 09:37:37 +0300

    completed mobile menu

--- dev/js/index.ts
+++ dev/js/index.ts
@@ -7,11 +7,12 @@ import { loadingBtn } from "./loadingBtn"
 import { compensateForScrollbar } from "./compensateForScrollbar"
 import { initModals } from "./modals"
 import { initDropdowns } from "./dropdown"
-import { lazyLoad } from "./lazyLoad";
+import { lazyLoad } from "./lazyLoad"
 // import { initAccordions } from "./accordion"
 import { initIntlTel } from "./intlInput"
-import { accordions } from "./accordions";
-import { tooltips } from "./tooltips";
+import { accordions } from "./accordions"
+import { tooltips } from "./tooltips"
+import { initMenu } from "./menu"
 
 let IS_INITED = false
 
@@ -36,6 +37,7 @@ export const dynamicFunctions = (context?: HTMLElement) => {
 
 const staticFunctions = () => {
     compensateForScrollbar()
+    initMenu()
 }
 
 window.dynamicFunctions = dynamicFunctions
